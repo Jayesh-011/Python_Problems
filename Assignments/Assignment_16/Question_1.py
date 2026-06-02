88810f3a83cd096a0da5c44f0a2b96ecafc1a45e
aorus_01 <patiljayesh9923@gmail.com>
2026-06-02 23:59:39 +0530

    Remove duplicate folder

--- Assignments/Assignment_16/Question_1.py
+++
@@ -1,9 +0,0 @@
-def fun():
-    print("Hello from fun")
-
-def main():
-    print("Inside Main")
-    fun()
-
-if __name__ == "__main__":
-    main()
--- Assignments/Assignment_16/Question_4.py
+++
@@ -1,9 +0,0 @@
-def printing():
-    for i in range(5):
-        print("Marvellous")
-
-def main():
-    printing()
-
-if __name__ == "__main__":
-    main()
